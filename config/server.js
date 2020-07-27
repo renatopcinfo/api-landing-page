ff0f8fa89b69dd74d0ffffc0a30216649eedeec6
renatopcinfo <renatopcinfo@gmail.com>
2020-07-27 00:08:26 -0300

    strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7cd27842a4818d79711c9d324e500b0f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2a58504d-7af6-43b1-bbae-359acf2cdbf9'
+};
