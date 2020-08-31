11c0fdf16d476b40ddaee66d4eb84c4849b44926
renatopcinfo <renatopcinfo@gmail.com>
2020-08-31 00:16:11 -0300

    update strap

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '7cd27842a4818d79711c9d324e500b0f'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
